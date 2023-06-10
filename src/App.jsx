47a83e52bb4043594ecf5c35957723199e25c296
Lucio Rubens <luciorubeens@users.noreply.github.com>
2023-06-10 15:24:33 -0300

    Implement pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import './App.css'
+import { Container } from "@mui/material";
+import { Router } from "./Router";
 
 export default function App() {
   return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit 🌀
-    </main>
-  )
+    <Container maxWidth="md" sx={{ flex: 1, display: "flex", alignItems: "center", justifyContent: "center" }}>
+      <Router />
+    </Container>
+  );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-)
+import { CssBaseline } from "@mui/material";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CssBaseline />
+    <App />
+  </React.StrictMode>
+);
